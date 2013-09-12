997c0d9452a2ebbc1ce8bad1e32ff316a3e066aa
Nick Such <nicksuch@gmail.com>
2013-09-12 13:36:43 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{	
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+	private int numberOfGameObjects;
+	
+	void Start()
+	{
+		count = 0;
+		SetCountText();
+		winText.text = "";
+		numberOfGameObjects = GameObject.FindGameObjectsWithTag("PickUp").Length;
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+	
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString();
+		if(count >= numberOfGameObjects)
+		{
+			winText.text = "YOU WIN!";
+		}
+	}
+}
